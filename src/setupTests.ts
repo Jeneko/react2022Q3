bdff9801abb51fcff03ca83ffd106b4a22b14fe0
Jeneko <jenekomail@gmail.com>
2022-10-02 17:46:37 +0300

    feat: mock localStorage

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,48 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+interface Store {
+  [key: string]: string;
+}
+
+class LocalStorageMock {
+  store: Store;
+  length: number;
+
+  constructor() {
+    this.store = {};
+    this.length = 0;
+  }
+
+  clear(): void {
+    this.store = {};
+    this.length = 0;
+  }
+
+  key(n: number): string | null {
+    if (typeof n === 'undefined') {
+      throw new Error('Argument is not presented');
+    }
+
+    if (n >= Object.keys(this.store).length) {
+      return null;
+    }
+
+    return Object.keys(this.store)[n];
+  }
+
+  getItem(key: string): string | null {
+    return this.store[key] || null;
+  }
+
+  setItem(key: string, value: string): void {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key: string): void {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
