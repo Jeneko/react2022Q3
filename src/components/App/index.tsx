8fc6f1b666b95c40c49f1cec09cd9f83a79429a4
Jeneko <jenekomail@gmail.com>
2022-10-02 17:20:24 +0300

    feat: implement About page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { DefaultLayout } from 'components/Layout/DefaultLayout';
 import { MainPage } from 'pages/Main';
+import { AboutPage } from 'pages/About';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<MainPage />} />
+          <Route path="about" element={<AboutPage />} />
         </Route>
       </Routes>
     </>
---
+++ src/pages/About/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { PageHeading } from 'components/PageHeading';
+
+function AboutPage() {
+  return (
+    <div className="about-page container">
+      <PageHeading>About Page</PageHeading>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis rem quia nihil,
+        reiciendis, provident mollitia recusandae repudiandae, ipsam aliquam quis asperiores
+        eligendi. Mollitia amet rem minima reiciendis eligendi, nobis id? Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Blanditiis rem quia nihil, reiciendis, provident mollitia
+        recusandae repudiandae, ipsam aliquam quis asperiores eligendi.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis rem quia nihil,
+        reiciendis, provident mollitia recusandae repudiandae, ipsam aliquam quis asperiores
+        eligendi. Mollitia amet rem minima reiciendis eligendi, nobis id?
+      </p>
+    </div>
+  );
+}
+
+export { AboutPage };
