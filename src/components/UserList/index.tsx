40d25dc0f5b97c6fc3d669bc6b4d55c895b5a2c5
Jeneko <jenekomail@gmail.com>
2022-10-05 15:03:50 +0300

    feat: implement UserList component

---
+++ src/components/UserList/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { UserCard, UserCardProps } from 'components/UserCard';
+import './UserList.css';
+
+interface UserListProps {
+  cards: UserCardProps[];
+}
+
+class UserList extends React.Component<UserListProps> {
+  render() {
+    return (
+      <div className="form-card-list">
+        {this.props.cards.map((card, i) => (
+          <UserCard {...card} key={i} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export { UserList };
