c66e6a987c967160624d7235bd3336fcab8d3e4a
Jeneko <jenekomail@gmail.com>
2022-10-05 15:50:44 +0300

    feat: implement Form page full functionality

--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import { PageHeading } from 'components/PageHeading';
+import { CreateUserForm } from 'components/CreateUserForm';
+import { UserList } from 'components/UserList';
+import { UserCardProps } from 'components/UserCard';
+import './Form.css';
 
-class FormPage extends React.Component {
+interface FormPageState {
+  cards: UserCardProps[];
+}
+
+class FormPage extends React.Component<object, FormPageState> {
+  constructor(props: object) {
+    super(props);
+    this.state = { cards: [] };
+    this.addFormCard = this.addFormCard.bind(this);
+  }
+
+  addFormCard(card: UserCardProps) {
+    this.setState({ cards: [...this.state.cards, card] });
+  }
+
   render() {
     return (
       <div className="form-page container">
         <PageHeading>Form Page</PageHeading>
+        <div className="form-page__content">
+          <div className="form-page__form">
+            <CreateUserForm onFormSubmit={this.addFormCard} />
+          </div>
+          <div className="form-page__list">
+            <h2 className="form-page__list-heading">User List</h2>
+            {Boolean(this.state.cards.length) ? (
+              <UserList cards={this.state.cards} />
+            ) : (
+              <p>Create new user to show in the list</p>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
