41dbe4b2a88c667c3dca8f6ec8f696bb353f99f0
Jeneko <jenekomail@gmail.com>
2022-10-12 16:29:28 +0300

    feat: add animation svg to Spinner component

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import spinnerUrl from './spinner.svg';
 import './Spinner.css';
 
 function Spinner() {
   return (
     <div className="spinner">
       <div className="spinner__content">
+        <img className="spinner__circle" src={spinnerUrl} title="" />
         <div className="spinner__text">Loading...</div>
       </div>
     </div>
