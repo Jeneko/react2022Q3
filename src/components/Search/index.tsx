c086c3ebf0915c4ca5c1b6e0528b47e6667406c6
Jeneko <jenekomail@gmail.com>
2022-10-15 17:47:47 +0300

    feat: update Search components logic for searching via flickr API

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,26 +1,44 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, KeyboardEvent } from 'react';
 import './Search.css';
 
 export const LOCAL_STORAGE_KEY = 'searchValue';
+
+interface SearchProps {
+  onEnter: (value: string) => void;
+}
 
 interface SearchState {
   value: string;
 }
 
-class Search extends React.Component<object, SearchState> {
-  constructor(props: object) {
+class Search extends React.Component<SearchProps, SearchState> {
+  constructor(props: SearchProps) {
     super(props);
     this.state = { value: localStorage.getItem(LOCAL_STORAGE_KEY) || '' };
     this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onKeyDownHandler = this.onKeyDownHandler.bind(this);
   }
 
   componentWillUnmount(): void {
     localStorage.setItem(LOCAL_STORAGE_KEY, this.state.value);
   }
 
-  onChangeHandler(e: ChangeEvent) {
+  onKeyDownHandler(e: KeyboardEvent): void {
+    if (e.key === 'Enter') {
+      const trimmedValue = this.state.value.trim();
+      localStorage.setItem(LOCAL_STORAGE_KEY, trimmedValue);
+      this.setState({ value: trimmedValue });
+      this.props.onEnter(trimmedValue);
+    }
+  }
+
+  onChangeHandler(e: ChangeEvent): void {
     const target = e.target as HTMLInputElement;
     this.setState({ value: target.value });
+
+    if (target.value.trim() === '') {
+      this.props.onEnter('');
+    }
   }
 
   render() {
@@ -31,6 +49,7 @@ class Search extends React.Component<object, SearchState> {
             type="search"
             className="search__input"
             onChange={this.onChangeHandler}
+            onKeyDown={this.onKeyDownHandler}
             placeholder="Search..."
             value={this.state.value}
           />
