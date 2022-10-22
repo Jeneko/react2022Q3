f98bc2b5cf3cdb85014fe0dd827e6d19e9876822
Jeneko <jenekomail@gmail.com>
2022-10-22 21:32:21 +0300

    refactor: rewrite FlickrFullInfo as functional component

--- src/components/FlickrFullInfo/index.tsx
+++ src/components/FlickrFullInfo/index.tsx
@@ -2,47 +2,45 @@ import React from 'react';
 import { FlickrPhoto, getPhotoUrl } from 'API/flickr';
 import './FlickrFullInfo.css';
 
-class FlickrFullInfo extends React.Component<FlickrPhoto> {
-  render() {
-    const { server, id, secret, title } = this.props;
-    const imgUrl = getPhotoUrl(server, id, secret);
-    const owner = this.props.ownername ? this.props.ownername : '';
-    const date = this.props.dateupload ? new Date(Number(this.props.dateupload)) : '';
-    const content = this.props.description?._content ? this.props.description?._content : '';
+function FlickrFullInfo(props: FlickrPhoto) {
+  const { server, id, secret, title } = props;
+  const imgUrl = getPhotoUrl(server, id, secret);
+  const owner = props.ownername ? props.ownername : '';
+  const date = props.dateupload ? new Date(Number(props.dateupload)) : '';
+  const content = props.description?._content ? props.description?._content : '';
 
-    return (
-      <div className="flickr-full-info">
-        <h3 className="flickr-full-info__title">{title}</h3>
-        <img className="flickr-full-info__img" src={imgUrl} alt={title} />
+  return (
+    <div className="flickr-full-info">
+      <h3 className="flickr-full-info__title">{title}</h3>
+      <img className="flickr-full-info__img" src={imgUrl} alt={title} />
 
-        {content && (
-          <div
-            className="flickr-full-info__desc"
-            dangerouslySetInnerHTML={{ __html: content }}
-            data-testid="desc"
-          ></div>
-        )}
+      {content && (
+        <div
+          className="flickr-full-info__desc"
+          dangerouslySetInnerHTML={{ __html: content }}
+          data-testid="desc"
+        ></div>
+      )}
 
-        {(owner || date) && (
-          <div className="flickr-full-info__addition">
-            {owner && (
-              <div className="flickr-full-info__owner" data-testid="owner">
-                <b>Owner: </b>
-                <span>{owner}</span>
-              </div>
-            )}
+      {(owner || date) && (
+        <div className="flickr-full-info__addition">
+          {owner && (
+            <div className="flickr-full-info__owner" data-testid="owner">
+              <b>Owner: </b>
+              <span>{owner}</span>
+            </div>
+          )}
 
-            {date && (
-              <div className="flickr-full-info__date" data-testid="date">
-                <b>Uploaded: </b>
-                <span>{date.toLocaleDateString()}</span>
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-    );
-  }
+          {date && (
+            <div className="flickr-full-info__date" data-testid="date">
+              <b>Uploaded: </b>
+              <span>{date.toLocaleDateString()}</span>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export { FlickrFullInfo };
