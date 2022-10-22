5264d38a789c3c1d59872a76d387bb12316cf814
Jeneko <jenekomail@gmail.com>
2022-10-22 21:34:04 +0300

    refactor: rewrite Search as functional component

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, KeyboardEvent } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
 import './Search.css';
 
 export const LOCAL_STORAGE_KEY = 'searchValue';
@@ -7,57 +7,46 @@ interface SearchProps {
   onEnter: (value: string) => void;
 }
 
-interface SearchState {
-  value: string;
-}
+function Search(props: SearchProps) {
+  const [value, setValue] = useState(localStorage.getItem(LOCAL_STORAGE_KEY) || '');
 
-class Search extends React.Component<SearchProps, SearchState> {
-  constructor(props: SearchProps) {
-    super(props);
-    this.state = { value: localStorage.getItem(LOCAL_STORAGE_KEY) || '' };
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-    this.onKeyDownHandler = this.onKeyDownHandler.bind(this);
-  }
+  useEffect(() => () => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, value);
+  });
 
-  componentWillUnmount(): void {
-    localStorage.setItem(LOCAL_STORAGE_KEY, this.state.value);
-  }
-
-  onKeyDownHandler(e: KeyboardEvent): void {
+  function onKeyDownHandler(e: KeyboardEvent): void {
     if (e.key === 'Enter') {
-      const trimmedValue = this.state.value.trim();
+      const trimmedValue = value.trim();
       localStorage.setItem(LOCAL_STORAGE_KEY, trimmedValue);
-      this.setState({ value: trimmedValue });
-      this.props.onEnter(trimmedValue);
+      setValue(trimmedValue);
+      props.onEnter(trimmedValue);
     }
   }
 
-  onChangeHandler(e: ChangeEvent): void {
+  function onChangeHandler(e: ChangeEvent): void {
     const target = e.target as HTMLInputElement;
-    this.setState({ value: target.value });
+    setValue(target.value);
 
     if (target.value.trim() === '') {
-      this.props.onEnter('');
+      props.onEnter('');
     }
   }
 
-  render() {
-    return (
-      <div className="search-container">
-        <div className="search">
-          <input
-            type="search"
-            className="search__input"
-            onChange={this.onChangeHandler}
-            onKeyDown={this.onKeyDownHandler}
-            placeholder="Search..."
-            value={this.state.value}
-          />
-          <div className="search__icon"></div>
-        </div>
+  return (
+    <div className="search-container">
+      <div className="search">
+        <input
+          type="search"
+          className="search__input"
+          onChange={onChangeHandler}
+          onKeyDown={onKeyDownHandler}
+          placeholder="Search..."
+          value={value}
+        />
+        <div className="search__icon"></div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export { Search };
