b2de627139587a5398f83b276f594b58fef76d8e
Jeneko <jenekomail@gmail.com>
2022-10-22 21:36:05 +0300

    refactor: rewrite Main as functional component

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Spinner } from 'components/Spinner';
 import { CardsList } from 'components/CardList';
 import { PageHeading } from 'components/PageHeading';
@@ -7,80 +7,73 @@ import { FlickrPhoto, flickrPhotoSearch } from 'API/flickr';
 import { FlickrFullInfo } from 'components/FlickrFullInfo';
 import { Modal } from 'components/Modal';
 
-interface MainPageState {
-  photos: FlickrPhoto[];
-  fullInfoPhotoId: string;
-  showLoading: boolean;
-  showModal: boolean;
-}
+function MainPage() {
+  const [photos, setPhotos] = useState([] as FlickrPhoto[]);
+  const [showLoading, setShowLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [fullInfoPhotoId, setFullInfoPhotoId] = useState('');
 
-class MainPage extends React.Component<object, MainPageState> {
-  constructor(props: object) {
-    super(props);
-    this.state = { photos: [], showLoading: false, showModal: false, fullInfoPhotoId: '' };
-    this.updatePhotos = this.updatePhotos.bind(this);
-  }
-
-  async componentDidMount(): Promise<void> {
+  useEffect(() => {
     const search = localStorage.getItem(LOCAL_STORAGE_KEY) || '';
-    this.updatePhotos(search);
-  }
+    updatePhotos(search);
+  }, []);
 
-  async updatePhotos(search: string): Promise<void> {
+  async function updatePhotos(search: string): Promise<void> {
     if (!search) {
-      this.setState({ photos: [], showLoading: false });
+      setPhotos([]);
+      setShowLoading(false);
       return;
     }
 
-    this.setState({ showLoading: true });
-    const photos = await flickrPhotoSearch(search);
-    this.setState({ photos, showLoading: false });
+    setShowLoading(true);
+    const curPhotos = await flickrPhotoSearch(search);
+    setPhotos(curPhotos);
+    setShowLoading(false);
   }
 
-  getPhotoById(id: string): FlickrPhoto | undefined {
-    return this.state.photos.find((photo) => photo.id === id);
+  function getPhotoById(id: string): FlickrPhoto | undefined {
+    return photos.find((photo) => photo.id === id);
   }
 
-  render() {
-    let selectedPhoto;
+  let selectedPhoto;
 
-    if (this.state.showModal && this.state.fullInfoPhotoId) {
-      selectedPhoto = this.getPhotoById(this.state.fullInfoPhotoId);
-    }
+  if (showModal && fullInfoPhotoId) {
+    selectedPhoto = getPhotoById(fullInfoPhotoId);
+  }
 
-    return (
-      <div className="main">
-        {this.state.showModal && selectedPhoto && (
-          <Modal onClose={() => this.setState({ showModal: false })}>
-            <FlickrFullInfo {...selectedPhoto} />
-          </Modal>
-        )}
+  return (
+    <div className="main">
+      {showModal && selectedPhoto && (
+        <Modal onClose={() => setShowModal(false)}>
+          <FlickrFullInfo {...selectedPhoto} />
+        </Modal>
+      )}
 
-        {this.state.showLoading && <Spinner />}
+      {showLoading && <Spinner />}
 
-        <div className="container">
-          <PageHeading>Main Page</PageHeading>
-        </div>
-
-        <Search onEnter={this.updatePhotos} />
-
-        <div className="container">
-          <CardsList
-            cards={this.state.photos}
-            onShowMore={(photoId: string) => {
-              this.setState({ fullInfoPhotoId: photoId, showModal: true });
-            }}
-          />
-        </div>
-
-        {!this.state.photos.length && (
-          <p style={{ textAlign: 'center' }}>
-            Please, enter your <b>search request</b> in the&nbsp;field above
-          </p>
-        )}
+      <div className="container">
+        <PageHeading>Main Page</PageHeading>
       </div>
-    );
-  }
+
+      <Search onEnter={updatePhotos} />
+
+      <div className="container">
+        <CardsList
+          cards={photos}
+          onShowMore={(photoId: string) => {
+            setFullInfoPhotoId(photoId);
+            setShowModal(true);
+          }}
+        />
+      </div>
+
+      {!photos.length && (
+        <p style={{ textAlign: 'center' }}>
+          Please, enter your <b>search request</b> in the&nbsp;field above
+        </p>
+      )}
+    </div>
+  );
 }
 
 export { MainPage };
