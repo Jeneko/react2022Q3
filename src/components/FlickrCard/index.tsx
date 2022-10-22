50ba3d050180b2839112b3bd136ab16ec0c7f98a
Jeneko <jenekomail@gmail.com>
2022-10-22 21:27:28 +0300

    refactor: rewrite FlickrCard as functional component

--- src/components/FlickrCard/index.tsx
+++ src/components/FlickrCard/index.tsx
@@ -7,43 +7,37 @@ interface FlickrCardProps extends FlickrPhoto {
   onShowMore: (id: string) => void;
 }
 
-class FlickrCard extends React.Component<FlickrCardProps> {
-  constructor(props: FlickrCardProps) {
-    super(props);
-  }
+function FlickrCard(props: FlickrCardProps) {
+  const { server, id, secret, title } = props;
+  const cardClass = 'flickr-card';
+  const imgUrl = getPhotoUrl(server, id, secret);
 
-  render() {
-    const { server, id, secret, title } = this.props;
-    const cardClass = 'flickr-card';
-    const imgUrl = getPhotoUrl(server, id, secret);
+  return (
+    <article className={cardClass}>
+      <div className="flickr-card__img-container" title="More info">
+        <img
+          className="flickr-card__img"
+          src={imgUrl}
+          alt={title}
+          onClick={() => props.onShowMore(id)}
+          data-testid="flickr-card__img"
+        />
+      </div>
 
-    return (
-      <article className={cardClass}>
-        <div className="flickr-card__img-container" title="More info">
-          <img
-            className="flickr-card__img"
-            src={imgUrl}
-            alt={title}
-            onClick={() => this.props.onShowMore(id)}
-            data-testid="flickr-card__img"
-          />
-        </div>
+      <h3 className="flickr-card__title" title={title}>
+        {title}
+      </h3>
 
-        <h3 className="flickr-card__title" title={title}>
-          {title}
-        </h3>
-
-        <div className="flickr-card__buttons">
-          <Button
-            text="More info"
-            title="More info"
-            type={ButtonType.primary}
-            onClick={() => this.props.onShowMore(id)}
-          />
-        </div>
-      </article>
-    );
-  }
+      <div className="flickr-card__buttons">
+        <Button
+          text="More info"
+          title="More info"
+          type={ButtonType.primary}
+          onClick={() => props.onShowMore(id)}
+        />
+      </div>
+    </article>
+  );
 }
 
 export { FlickrCard };
