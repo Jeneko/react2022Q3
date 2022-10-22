fb7997d887eb977124feeeea7bf4d84648f1a7e0
Jeneko <jenekomail@gmail.com>
2022-10-22 21:37:47 +0300

    refactor: rewrite Form page as functional component

--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -1,45 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PageHeading } from 'components/PageHeading';
 import { CreateUserForm } from 'components/CreateUserForm';
 import { UserList } from 'components/UserList';
 import { UserCardProps } from 'components/UserCard';
 import './Form.css';
 
-interface FormPageState {
-  cards: UserCardProps[];
-}
+function FormPage() {
+  const [cards, setCards] = useState([] as UserCardProps[]);
 
-class FormPage extends React.Component<object, FormPageState> {
-  constructor(props: object) {
-    super(props);
-    this.state = { cards: [] };
-    this.addFormCard = this.addFormCard.bind(this);
+  function addFormCard(card: UserCardProps) {
+    setCards([...cards, card]);
   }
 
-  addFormCard(card: UserCardProps) {
-    this.setState({ cards: [...this.state.cards, card] });
-  }
-
-  render() {
-    return (
-      <div className="form-page container" data-testid="form-page">
-        <PageHeading>Form Page</PageHeading>
-        <div className="form-page__content">
-          <div className="form-page__form">
-            <CreateUserForm onFormSubmit={this.addFormCard} />
-          </div>
-          <div className="form-page__list">
-            <h2 className="form-page__list-heading">User List</h2>
-            {Boolean(this.state.cards.length) ? (
-              <UserList cards={this.state.cards} />
-            ) : (
-              <p>Create new user to show in the list</p>
-            )}
-          </div>
+  return (
+    <div className="form-page container" data-testid="form-page">
+      <PageHeading>Form Page</PageHeading>
+      <div className="form-page__content">
+        <div className="form-page__form">
+          <CreateUserForm onFormSubmit={addFormCard} />
+        </div>
+        <div className="form-page__list">
+          <h2 className="form-page__list-heading">User List</h2>
+          {Boolean(cards.length) ? (
+            <UserList cards={cards} />
+          ) : (
+            <p>Create new user to show in the list</p>
+          )}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export { FormPage };
