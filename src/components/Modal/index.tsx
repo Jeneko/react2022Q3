df325ced143616fbab85da1275358a73ed0afe3e
Jeneko <jenekomail@gmail.com>
2022-10-22 21:26:21 +0300

    refactor: rewrite Modal as functional component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,25 +7,23 @@ interface ModalProps {
   onClose: () => void;
 }
 
-class Modal extends React.Component<ModalProps> {
-  render() {
-    return (
-      <div className="modal" data-testid="modal-component">
-        <div className="modal__backdrop" onClick={this.props.onClose} />
-        <div className="modal__container">
-          <button
-            className="modal__close"
-            onClick={this.props.onClose}
-            title="Close"
-            data-testid="close-modal"
-          >
-            <img src={close} alt="" />
-          </button>
-          <div className="modal__content">{this.props.children}</div>
-        </div>
+function Modal(props: ModalProps) {
+  return (
+    <div className="modal" data-testid="modal-component">
+      <div className="modal__backdrop" onClick={props.onClose} />
+      <div className="modal__container">
+        <button
+          className="modal__close"
+          onClick={props.onClose}
+          title="Close"
+          data-testid="close-modal"
+        >
+          <img src={close} alt="" />
+        </button>
+        <div className="modal__content">{props.children}</div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export { Modal };
